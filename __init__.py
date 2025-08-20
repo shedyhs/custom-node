4f97e88fd6d68389844b4cb547d255936e1ec5f5
Shedy <30814200+shedyhs@users.noreply.github.com>
2025-08-20 09:12:49 -0400

    Create __init__.py

---
+++ __init__.py
@@ -0,0 +1,8 @@
+"""
+WebSocket Save with Prompt ID
+Custom node for ComfyUI
+"""
+
+from .websocket_save import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+
+__all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
