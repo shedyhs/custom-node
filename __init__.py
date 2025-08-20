15befb024be72320b82bab4a007b9b29677ca118
Shedy <30814200+shedyhs@users.noreply.github.com>
2025-08-20 09:50:53 -0400

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,6 @@ WebSocket Save with Prompt ID
 Custom node for ComfyUI
 """
 
-from .websocket_save import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
+from .nodes import NODE_CLASS_MAPPINGS, NODE_DISPLAY_NAME_MAPPINGS
 
 __all__ = ['NODE_CLASS_MAPPINGS', 'NODE_DISPLAY_NAME_MAPPINGS']
